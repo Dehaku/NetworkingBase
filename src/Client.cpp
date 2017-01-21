4cfe4c61cfdd9d021e2dd7f377fe39e2a3562df8
Johnny Fojtik <dehakuzedon@gmail.com>
2017-01-20 21:31:41 -0600

    Server and Client communicate

--- src/Client.cpp
+++ src/Client.cpp
@@ -8,6 +8,22 @@ sf::TcpSocket socket;
 
 
 
+void exchangeHellos()
+{
+    // Receive a message from the server
+    char in[128];
+    std::size_t received;
+    if (socket.receive(in, sizeof(in), received) != sf::Socket::Done)
+        return;
+    std::cout << "Message received from the server: \"" << in << "\"" << std::endl;
+
+    // Send an answer to the server
+    const char out[] = "Hi, I'm a client";
+    if (socket.send(out, sizeof(out)) != sf::Socket::Done)
+        return;
+    std::cout << "Message sent to the server: \"" << out << "\"" << std::endl;
+}
+
 
 void activateClient()
 {
@@ -18,6 +34,6 @@ void activateClient()
         std::cout << "Failed to connect to... \n" << IPAddress << ":" << serverPort << std::endl;
 
     std::cout << "Probably connected!";
-
+    exchangeHellos();
 
 }
--- src/Server.cpp
+++ src/Server.cpp
@@ -7,6 +7,21 @@ unsigned short port;
 int serverNum = 0;
 
 
+void exchangeHellos(sf::TcpSocket &socket)
+{
+    // Send a message to the connected client
+    const char out[] = "Hi, I'm the server";
+    if (socket.send(out, sizeof(out)) != sf::Socket::Done)
+        return;
+    std::cout << "Message sent to the client: \"" << out << "\"" << std::endl;
+
+    // Receive a message back from the client
+    char in[128];
+    std::size_t received;
+    if (socket.receive(in, sizeof(in), received) != sf::Socket::Done)
+        return;
+    std::cout << "Answer received from the client: \"" << in << "\"" << std::endl;
+}
 
 void serverListen()
 {
@@ -19,6 +34,8 @@ void serverListen()
     if (listener.accept(client) != sf::Socket::Done)
         std::cout << "Listener had an error while listening for a client. \n";
     std::cout << "Client connected: " << client.getRemoteAddress() << std::endl;
+
+    exchangeHellos(client);
 
 }
 
